0f345d3291c255a2ad2e032b0c72069ee2f25648
remmo1 <remigiusz.pisarski@onet.pl>
2025-12-05 17:41:14 +0100

    Add java parallel code

--- src/main/java/org/example/PiNumberBenchmark.java
+++ src/main/java/org/example/PiNumberBenchmark.java
@@ -5,16 +5,29 @@ import org.openjdk.jmh.annotations.*;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Warmup(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @Fork(2)
 @State(Scope.Benchmark)
 public class PiNumberBenchmark {
 
+    @Threads(1)
     @Benchmark
-    public void testA() {
-        MathOperations.countPI(100);
+    public void benchmarkPiOneThread() {
+        MathOperations.countPI(100_000_000);
+    }
+
+    @Threads(2)
+    @Benchmark
+    public void benchmarkPiTwoThreads() {
+        MathOperations.countPI(100_000_000);
+    }
+
+    @Threads(4)
+    @Benchmark
+    public void benchmarkPiFourThreads() {
+        MathOperations.countPI(100_000_000);
     }
 
 }
