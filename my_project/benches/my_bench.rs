0350aae758d0ee71750b2a3f006c5b4392b783e4
remmo1 <remigiusz.pisarski@onet.pl>
2025-12-05 17:21:15 +0100

    Add rust parallel

--- my_project/benches/my_bench.rs
+++ my_project/benches/my_bench.rs
@@ -1,10 +1,23 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn bench_my_method(c: &mut Criterion) {
+fn bench_leibniz(c: &mut Criterion) {
     c.bench_function("leibniz_pi 100M", |b| {
         b.iter(|| my_project::leibniz_pi(100_000_000))
     });
 }
 
-criterion_group!(benches, bench_my_method);
+fn bench_leibniz_parallel(c: &mut Criterion) {
+    let mut group = c.benchmark_group("leibniz_pi_parallel");
+
+    for threads in [2, 4, 6, 8] {
+        group.bench_with_input(format!("{threads} threads"), &threads, |b, &t| {
+            b.iter(|| my_project::leibniz_pi_parallel_manual(100_000_000, t))
+        });
+    }
+
+    group.finish();
+}
+
+
+criterion_group!(benches, bench_leibniz_parallel);
 criterion_main!(benches);
